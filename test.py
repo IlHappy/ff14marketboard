e25fc0972e124be3041fc68871988ecfcdad530d
Philippe Lejeune <philippelejeune01@hotmail.com>
2022-04-17 18:53:00 -0400

    small refactoring

--- test.py
+++ test.py
@@ -1,37 +1,26 @@
 import requests
 import pandas as pd
 
-class Item:
-    def __init__(self, id) -> None:
-        self.id = id 
-a = "https://universalis.app"
-r = requests.get(f"{a}/api/history/brynhildr/36070?entries=50")
-r.status_code
+#querying universalis endpoint
+base_url = "https://universalis.app"
+sales_history = requests.get(f"{base_url}/api/history/brynhildr/36070?entries=50")
+print(sales_history.json())
 
-r.headers['content-type']
-
-r.encoding
-
-r.text
-
-r.json()
-
-print(r.json())
-
-
-
-""" items = pd.read_csv("ffxiv-datamining/csv/Item.csv" ,header = 1 )
-
+#read item data from mining repo https://github.com/xivapi/ffxiv-datamining
+items = pd.read_csv("ffxiv-datamining/csv/Item.csv", header=1) 
 items = items.drop([0,2])
 
+#sorting for only endgame items
 items["Level{Item}"] = items["Level{Item}"].astype("int16")
 items = items.sort_values("Level{Item}", ascending = False)
-
 endgame_items = items.loc[items["Level{Item}"] > 560 ]
+
+#filtering to only tradable items
 endgame_items["IsUntradable"] = endgame_items["IsUntradable"].astype("boolean") 
 endgame_items["CanBeHq"] = endgame_items["CanBeHq"].astype("boolean") 
-
 endgame_items_marketable = endgame_items[~endgame_items["IsUntradable"]]
 endgame_items_marketable = endgame_items_marketable[endgame_items["CanBeHq"]]
+
+#output
 print(endgame_items_marketable.head())
-endgame_items_marketable.to_csv("marketable", index = False) """
+endgame_items_marketable.to_csv("marketable", index = False)
